d105be422363a654051ea072d8eb9d720decb788
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-10-06 18:05:53 +0200

    chore: bump template

--- src/eventic/__init__.py
+++ src/eventic/__init__.py
@@ -1,10 +1,16 @@
-"""Eventic package."""
+"""Eventic: Event emitters."""
 
 from __future__ import annotations
 
 from importlib.metadata import version
 
 __version__ = version("eventic")
-
+__title__ = "Eventic"
+__description__ = "Event emitters."
+__author__ = "Philipp Temminghoff"
+__author_email__ = "philipptemminghoff@googlemail.com"
+__copyright__ = "Copyright (c) 2025 Philipp Temminghoff"
+__license__ = "MIT"
+__url__ = "https://github.com/phil65/eventic"
 
 __all__ = ["__version__"]
