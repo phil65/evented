836aed9db1827f20b82727e6bac0c9dd8405c819
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-10-06 05:18:04 +0200

    refactor: simplify version loading - remove unnecessary exception handling

--- src/eventic/__init__.py
+++ src/eventic/__init__.py
@@ -4,10 +4,7 @@ from __future__ import annotations
 
 from importlib.metadata import version
 
-try:
-    __version__ = version("eventic")
-except Exception:
-    __version__ = "unknown"
+__version__ = version("eventic")
 
 
 __all__ = ["__version__"]
